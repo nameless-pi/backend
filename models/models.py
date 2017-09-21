7f08b23183873a07b441d3278dc3c242ba827a5d
gabrielCorreaCordeiro <gabrielcordeiro@alunos.utfpr.edu.br>
2017-09-21 11:11:33 -0300

    autoria

--- models/models.py
+++ models/models.py
@@ -1,3 +1,7 @@
+'''Modelos, feitos usando SQLAlchemy ORM, referentes à modelagem da base de dados.'''
+
+__author__ = 'Gabriel Cordeiro'
+
 from passlib.apps import custom_app_context as pwd_context
 from flask_sqlalchemy import SQLAlchemy
 
@@ -19,7 +23,7 @@ class CRUD():
 
 
 class Admin(db.Model, CRUD):
-	__tablename__ = 'admins'
+	__tablename__ = "admin"
 
 	id = db.Column(db.Integer, primary_key=True)
 	login = db.Column(db.String(80), nullable=False)
@@ -35,61 +39,12 @@ class Admin(db.Model, CRUD):
 	def verify_password(self, password):
 		return pwd_context.verify(password, self.password)
 
-	def __repr__(self):
-		return '<Admin %r>' % self.name
-
-
-
-class Usuario(db.Model, CRUD):
-	__tablename__ = "usuario"
-
-	id = db.Column(db.Integer, primary_key=True)
-	rfid = db.Column(db.String(16), nullable=False)
-	nome = db.Column(db.String(80), nullable=False)
-	email = db.Column(db.String(100), nullable=False)
-
-	def __init__(self, rfid, nome, email):
-		self.rfid = rfid
-		self.nome = nome
-		self.email = email
-
-
-class Horario(db.Model, CRUD):
-	__tablename__ = "horario"
-	id = db.Column(db.Integer, primary_key=True)
-	hora_inicio = db.Column(db.DateTime, nullable=False)
-	hora_fim = db.Column(db.DateTime, nullable=False)
-	dia = db.Column(db.String(15), nullable=False)
-	tipo_user = db.Column(db.String(15), nullable=False)
-	sync = db.Column(db.Boolean, nullable=False)
-	nome_sala = db.Column(db.String(4), db.ForeignKey('sala.nome'),nullable = False)   
-
-	def __init__(self, hora_fim, hora_inicio, dia, tipo_user, sync):
-		self.hora_fim = hora_fim
-		self.hora_inicio = hora_inicio
-		self.dia = dia
-		self.tipo_user = tipo_user
-		self.sync = sync
-
-
-
-
-class Sala(db.Model, CRUD):
-	__tablename__ = "sala"
-
-	nome = db.Column(db.String(4), primary_key=True)
-	horarios = db.relationship('Horario')
-
-	def __init__(self, nome, horarios):
-		self.nome = nome
-		self.horarios = horarios
-
 
 class Acesso(db.Model, CRUD):
 	__tablename__ = "acesso"
 
 	id = db.Column(db.Integer, primary_key=True)
-	id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'),  nullable=False)
+	id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
 	nome_sala = db.Column(db.String(4), db.ForeignKey('sala.nome'), nullable=False)
 	sync = db.Column(db.Boolean, nullable=False)
 	salas = db.relationship('Sala')
@@ -99,6 +54,26 @@ class Acesso(db.Model, CRUD):
 		self.sync = sync
 		self.salas = salas
 		self.usuario = usuario
+
+
+class Horario(db.Model, CRUD):
+	__tablename__ = "horario"
+
+	id = db.Column(db.Integer, primary_key=True)
+	hora_inicio = db.Column(db.DateTime, nullable=False)
+	hora_fim = db.Column(db.DateTime, nullable=False)
+	dia = db.Column(db.String(15), nullable=False)
+	tipo_user = db.Column(db.String(15), nullable=False)
+	sync = db.Column(db.Boolean, nullable=False)
+	nome_sala = db.Column(db.String(4), db.ForeignKey('sala.nome'), nullable=False)
+
+	def __init__(self, nome_sala, hora_fim, hora_inicio, dia, tipo_user, sync):
+		self.nome_sala = nome_sala
+		self.hora_fim = hora_fim
+		self.hora_inicio = hora_inicio
+		self.dia = dia
+		self.tipo_user = tipo_user
+		self.sync = sync
 
 
 class Relatorio(db.Model, CRUD):
@@ -113,3 +88,27 @@ class Relatorio(db.Model, CRUD):
 	def __init__(self, tipo_in_out, horario):
 		self.tipo_in_out = tipo_in_out
 		self.horario = horario
+
+
+class Sala(db.Model, CRUD):
+	__tablename__ = "sala"
+
+	nome = db.Column(db.String(4), primary_key=True)
+	horarios = db.relationship('Horario')
+
+	def __init__(self, nome):
+		self.nome = nome
+
+
+class Usuario(db.Model, CRUD):
+	__tablename__ = "usuario"
+
+	id = db.Column(db.Integer, primary_key=True)
+	nome = db.Column(db.String(80), nullable=False)
+	email = db.Column(db.String(100), nullable=False)
+	rfid = db.Column(db.String(16), nullable=False)
+
+	def __init__(self, nome, email, rfid):
+		self.nome = nome
+		self.email = email
+		self.rfid = rfid
