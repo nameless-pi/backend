cbd5d4a7ac170ed918e210a1f0c38eb7f52127ae
kaioduarte <duartedossantos@gmail.com>
2017-09-23 19:16:48 -0300

    added api prefix and JWT authentication

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt import JWT
 
 
 app = Flask(__name__)
@@ -9,3 +10,19 @@ api = Api(app)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+
+from admin_model import Admin
+
+
+def authenticate(login, password):
+	login = Admin.query.filter(Admin.login == login).first()
+	if login and login.verify_password(password):
+		return login
+
+
+def identity(payload):
+	user_id = payload['identity']
+	return Admin.query.get(user_id).login
+
+
+jwt = JWT(app, authenticate, identity)
