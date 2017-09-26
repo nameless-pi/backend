af69e17bc9d8dab9da5d53bceeb53fd92a7a154f
kaioduarte <duartedossantos@gmail.com>
2017-09-25 21:47:20 -0300

    remove admin model

--- models.py
+++ models.py
@@ -1,24 +1,4 @@
-from passlib.apps import custom_app_context as pwd_context
-
 from base import CRUD, db
-
-
-class Admin(db.Model, CRUD):
-	__tablename__ = "admin"
-
-	id = db.Column(db.Integer, primary_key=True)
-	login = db.Column(db.String(80), nullable=False)
-	password = db.Column(db.String(128), nullable=False)
-
-	def __init__(self, login, password):
-		self.login = login
-		self.password = password
-
-	def hash_password(self, password):
-		self.password = pwd_context.encrypt(password)
-
-	def verify_password(self, password):
-		return pwd_context.verify(password, self.password)
 
 
 class Relatorio(db.Model, CRUD):
