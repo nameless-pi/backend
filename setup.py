bc932bdb90dcdef163f47625f6fd2934f50e4447
kaioduarte <duartedossantos@gmail.com>
2017-10-11 12:34:32 -0300

    clean up CORS

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt import JWT
-# from flask_cors import CORS
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -10,20 +10,8 @@ api = Api(app)
 
 app.config.from_object("config")
 
-# cors = CORS(app)
+cors = CORS(app)
 db = SQLAlchemy(app)
-
-
-@app.after_request
-def after_request(response):
-	response.headers.add("Access-Control-Allow-Origin", "*")
-	if request.method == "OPTIONS":
-		response.headers.add("Access-Control-Allow-Headers", "*")
-		response.headers.add("Access-Control-Allow-Methods", "DELETE, GET, POST, PUT")
-		headers = request.headers.get("Access-Control-Request-Headers")
-		if headers:
-			response.headers.add("Access-Control-Allow-Headers", headers)
-	return response
 
 from admin_model import Admin
 
