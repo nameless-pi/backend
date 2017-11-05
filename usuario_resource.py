9947ed64a2e3c1d4eee056d9de73132947a79955
gabriel <gabrielcordeiro@alunos.utfpr.edu.br>
2017-11-05 16:20:07 -0200

    reutiliza dados de um usuario anteriormente excluido

--- usuario_resource.py
+++ usuario_resource.py
@@ -22,8 +22,17 @@ class UsuarioResource(Resource):
 			response.status_code = 404
 			return respons
 		direitos_de_acesso = DireitoAcesso.query.filter(DireitoAcesso.alive == True,DireitoAcesso.id_usuario == id).all()
-		user = [{"id":getattr(user_query,"id"),"nome":getattr(user_query,"nome"),"tipo":getattr(user_query,"tipo"),"email":getattr(user_query,"email"),"rfid":getattr(user_query,"rfid"),
-	   			"direito_acesso":direitos_de_acesso,"last_update":getattr(user_query,"last_update"),"alive":getattr(user_query,"alive")}]
+		user = {
+				"id":getattr(user_query,"id"),
+				"nome":getattr(user_query,"nome"),
+				"tipo":getattr(user_query,"tipo"),
+				"email":getattr(user_query,"email"),
+				"rfid":getattr(user_query,"rfid"),
+				"direito_acesso":direitos_de_acesso,
+				"last_update":getattr(user_query,"last_update"),
+				"alive":getattr(user_query,"alive")
+			  }
+				
 		return schema.dump(user).data
 
 	@jwt_required()
@@ -94,8 +103,14 @@ class UsuarioResource(Resource):
 		user.last_update = datetime.now()
 		user.update()
 		direito_acesso_novo = DireitoAcesso.query.filter(DireitoAcesso.alive == True, DireitoAcesso.id_usuario == id).all()
-		new_user = {"id":getattr(user,"id"),"nome":getattr(user,"nome"),"tipo":getattr(user,"tipo"),"email":getattr(user,"email"),"rfid":getattr(user,"rfid"),
-	   			"direito_acesso":direito_acesso_novo,"last_update":getattr(user,"last_update"),"alive":getattr(user,"alive")}
+		new_user = {"id":getattr(user,"id"),
+					"nome":getattr(user,"nome"),
+					"tipo":getattr(user,"tipo"),
+					"email":getattr(user,"email"),
+					"rfid":getattr(user,"rfid"),
+					"direito_acesso":direito_acesso_novo,
+					"last_update":getattr(user,"last_update"),
+					"alive":getattr(user,"alive")}
 		return schema.dump(new_user).data
 
 	@jwt_required()
@@ -124,11 +139,24 @@ class UsuarioListResource(Resource):
 	@jwt_required()
 	def get(self):
 		users_query = Usuario.query.filter(Usuario.alive == True).all()
-		
-		usuarios = [{"id":getattr(i,"id"),"nome":getattr(i,"nome"),"tipo":getattr(i,"tipo"),"email":getattr(i,"email"),"rfid":getattr(i,"rfid"),
-	   			"direito_acesso":getattr(i,"direito_acesso"),"last_update":getattr(i,"last_update"),"alive":getattr(i,"alive")} for i in users_query]
+
+		usuarios = [
+			{
+				"id": getattr(i, "id"),
+				"nome": getattr(i, "nome"),
+				"tipo": getattr(i, "tipo"),
+				"email": getattr(i, "email"),
+				"rfid": getattr(i, "rfid"),
+				"direito_acesso": getattr(i, "direito_acesso"),
+				"last_update": getattr(i, "last_update"),
+				"alive": getattr(i, "alive")
+			} for i in users_query]
+
 		for usuario in usuarios:
-	   		usuario["direito_acesso"] = DireitoAcesso.query.filter(DireitoAcesso.alive == True, DireitoAcesso.id_usuario == usuario["id"]).all()
+			usuario["direito_acesso"] = DireitoAcesso.query.filter(
+				DireitoAcesso.alive == True,
+				DireitoAcesso.id_usuario == usuario["id"]
+			).all()
 		results = schema.dump(usuarios, many=True).data
 		return results
 
@@ -139,34 +167,97 @@ class UsuarioListResource(Resource):
 		parser.add_argument("rfid", type=str, required=True, location="json")
 		parser.add_argument("tipo", type=str, required=True, location="json")
 		parser.add_argument("email", type=str, required=True, location="json")
-
 		parser.add_argument("direito_acesso", action="append", location="json")
+
 		args = parser.parse_args(strict=True)
+		id_user = db.session.query(Usuario.id).filter(
+			Usuario.email == args["email"],
+			Usuario.alive == False
+		).all()
 
-		try:
-			user = Usuario(args["nome"], args["email"], args["rfid"], args["tipo"])
-			user.add(user)
-			if args["direito_acesso"]:
-				try:
-					for sala in args["direito_acesso"]:
-						# [{"id": 1, "nome": "E001"}]
-						sala = literal_eval(sala)
-						acesso = DireitoAcesso(user.id, sala["id_sala"])
-						acesso.add(acesso)
-				except SQLAlchemyError as e:
-					db.session.rollback()
-					resp = jsonify({"error": str(e)})
-					resp.status_code = 403
-					return resp
-			query = Usuario.query.get(user.id)
-
-		except SQLAlchemyError as e:
-			db.session.rollback()
-			resp = jsonify({"error": str(e)})
-			resp.status_code = 403
-			return resp
+		if id_user:
+			new_user = self.resurrect(id_user[0][0], args)
+			return schema.dump(new_user).data, 201, {"location": "api/v1/usuarios/" + str(new_user["id"])}
 		else:
-			return schema.dump(query).data, 201, {"location": "api/v1/usuarios/" + str(user.id)}
+			try:
+				user = Usuario(args["nome"], args["email"], args["rfid"], args["tipo"])
+				user.add(user)
+				if args["direito_acesso"]:
+					try:
+						for sala in args["direito_acesso"]:
+							# [{"id": 1, "nome": "E001"}]
+							sala = literal_eval(sala)
+							acesso = DireitoAcesso(user.id, sala["id_sala"])
+							acesso.add(acesso)
+					except SQLAlchemyError as e:
+						db.session.rollback()
+						resp = jsonify({"error": str(e)})
+						resp.status_code = 403
+						return resp
+				query = Usuario.query.get(user.id)
+				print("QUERY >", query)
+			except SQLAlchemyError as e:
+				db.session.rollback()
+				resp = jsonify({"error": str(e)})
+				resp.status_code = 403
+				return resp
+			else:
+				return schema.dump(query).data, 201, {"location": "api/v1/usuarios/" + str(user.id)}
+
+	def resurrect(self, id, args):
+		user = Usuario.query.get(id)
+		user.alive = True
+		user.nome = args["nome"]
+		user.tipo = args["tipo"]
+
+		if args["rfid"] and user.rfid != args["rfid"]:
+			if db.session.query(Usuario).filter(Usuario.rfid == args["rfid"]).all():
+				response = jsonify({"message": "RFID existente"})
+				response.status_code = 403
+				return response
+			else:
+				user.rfid = args["rfid"]
+		# CASO NÃO TENHAM SALAS NO JSON, RETIRAR O ACESSO DE TODAS AS SALAS
+		if not args["direito_acesso"]:
+			for acesso in user.direito_acesso:
+				acesso.alive = False
+
+		# CASO HAJAM SALAS NO JSON E NO BANCO
+		# [{"id": 1, "nome": "E001"}]
+		elif args["direito_acesso"]:
+			args = [literal_eval(i)["id_sala"] for i in args["direito_acesso"]]  # id do JSON
+			acessos = [acesso.id_sala for acesso in user.direito_acesso]  # id do usuário
+			to_add = [i for i in args if i not in acessos] # tá no JSON, mas não no BANCO
+
+			for acesso in user.direito_acesso:
+				if acesso.id_sala in args and not acesso.alive:  # sala tá no JSON e BANCO, mas tá morto no banco
+					acesso.alive = True
+				elif acesso.id_sala not in args and acesso.alive: # sala não tá no JSON e está no BANCO como vivo
+					acesso.alive = False
+
+			for new_acesso in to_add:
+				acesso = DireitoAcesso(id, new_acesso)
+				acesso.add(acesso)
+
+		user.last_update = datetime.now()
+		user.update()
+		direito_acesso_novo = DireitoAcesso.query.filter(
+			DireitoAcesso.alive == True,
+			DireitoAcesso.id_usuario == id
+		).all()
+
+		new_user = {
+			"id": getattr(user, "id"),
+			"nome": getattr(user, "nome"),
+			"tipo": getattr(user, "tipo"),
+			"email": getattr(user, "email"),
+			"rfid": getattr(user, "rfid"),
+			"direito_acesso": direito_acesso_novo,
+			"last_update": getattr(user, "last_update"),
+			"alive": getattr(user, "alive")
+		}
+
+		return new_user
 
 	@jwt_required()
 	def delete(self):
